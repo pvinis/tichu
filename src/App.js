d99b9d6ef880c5269bf3e3a6292857016d73f1fc
Pavlos Vinieratos <pvinis@gmail.com>
2019-12-10 18:21:12 +0100

    works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Client } from 'boardgame.io/react';
+import TicTacToeBoard from './TicTacToeBoard';
 
-export default App;
+function IsVictory(cells) {
+	return cells[0] === '0'
+  }
+
+  // Return true if all `cells` are occupied.
+  function IsDraw(cells) {
+	return cells.filter(c => c === null).length == 0;
+  }
+
+
+  const TicTacToe = {
+	setup: () => ({ cells: Array(9).fill(null) }),
+
+	moves: {
+	  clickCell: (G, ctx, id) => {
+		if (G.cells[id] === null) {
+		  G.cells[id] = ctx.currentPlayer;
+		}
+	  },
+	},
+
+	endIf: (G, ctx) => {
+	  if (IsVictory(G.cells)) {
+		return { winner: ctx.currentPlayer };
+	  }
+	  if (IsDraw(G.cells)) {
+		return { draw: true };
+	  }
+	},
+  };
+
+export const App = Client({
+	game: TicTacToe,
+	board: TicTacToeBoard,
+});
