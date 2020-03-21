c1cdca593974039337c528ca44148cf175384833
Pavlos Vinieratos <pvinis@gmail.com>
2020-03-22 00:07:55 +0100

    more

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const Server = require('boardgame.io/server').Server
 
+const { __DEV__ } = require('./logic/config')
 const Tichu = require('./logic/game').Tichu
 
 
 const server = Server({ games: [Tichu] })
-server.run(8000)
+server.run(__DEV__ ? 8000 : process.env.PORT)
